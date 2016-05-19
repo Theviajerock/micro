caec1e3084d4026a1b521bb0ff19540be4d4036f
victor <victor@ilujdebian.sysdev.co>
2016-05-19 16:43:40 -0500

    openid auth added

--- appli/views.py
+++ appli/views.py
@@ -1,9 +1,30 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from appli import app
+from .forms import LoginForm
 
+@app.route('/login', methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+    	flash('Login requested for OpenId="%s", remember_me=%s' %
+    			(form.openid.data, str(form.remember_me.data)))
+    	return redirect('/index')
+    return render_template('login.html',
+    						title='Sign In', 
+    						form=form,
+                            providers=app.config['OPENID_PROVIDERS'])
+
+
+
+
+
+
+
+"""
 @app.route('/')
 @app.route('/index')
 def index():
     name = 'Victor'
     title = 'SysDev Blog'
     return render_template('index.html', name=name, title=title)
+"""
